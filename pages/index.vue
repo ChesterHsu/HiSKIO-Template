3b826f96f959118ca8ab663c52257445e51bd18f
徐嘉輝 <aa0909286667@gmail.com>
2021-03-08 22:04:41 +0800

    實作index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>123</div>
+  <div class="carousel">
+    <a href="#">
+      <img src="@/assets/images/carousel.png">
+    </a>
+  </div>
 </template>
 
 <script>
@@ -8,4 +12,10 @@ export default {
 
 }
 </script>
+<style scoped>
+.carousel{
+  width: 100%;
+  height: 536px;
+}
+</style>
 
